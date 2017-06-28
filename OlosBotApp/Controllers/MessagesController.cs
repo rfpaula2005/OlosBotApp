713bd449b89f6ad9f510ef5591bb6664dd76fa49
Rodrigo Fernandes de Paula <rodrigo.paula@olos.com.br>
2017-06-28 14:51:37 -0300

    Uso do Table Storage para autenticação

--- OlosBotApp/Controllers/MessagesController.cs
+++ OlosBotApp/Controllers/MessagesController.cs
@@ -4,12 +4,106 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Autofac;
+using System.Security.Claims;
+using System.Web;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Azure;
+using System.Configuration;
+
+
+
 
 namespace OlosBotApp
 {
-    [BotAuthentication]
+
+    public class AppEntity : TableEntity
+    {
+        public AppEntity(string AppId, string AppPassword)
+        {
+            this.PartitionKey = AppId;
+            this.RowKey = AppPassword;
+        }
+
+        public AppEntity() { }
+
+        public string BotId { get; set; }
+    }
+    /// <summary>
+    /// A sample ICredentialProvider that is configured by multiple MicrosoftAppIds and MicrosoftAppPasswords
+    /// </summary>
+    public class MultiCredentialProvider : ICredentialProvider
+    {
+        public Dictionary<string, string> Credentials = new Dictionary<string, string>
+        {
+            { "YOUR_MSAPP_ID_1", "YOUR_MSAPP_PASSWORD_1" },
+            { "YOUR_MSAPP_ID_2", "YOUR_MSAPP_PASSWORD_2" }
+        };
+
+        public Task<bool> IsValidAppIdAsync(string appId)
+        {
+            // Set storage Account
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            // Get a reference to a table named "peopleTable"
+            CloudTable table = tableClient.GetTableReference("OlosBotCredentials");
+
+            // Create the CloudTable if it does not exist
+            //await table.CreateIfNotExistsAsync();
+
+            // Create a new customer entity.
+            //AppEntity botApp1 = new AppEntity(ConfigurationManager.AppSettings["MicrosoftAppId"], ConfigurationManager.AppSettings["MicrosoftAppPassword"]);
+            //botApp1.BotId = ConfigurationManager.AppSettings["BotId"];
+
+            // Create the TableOperation that inserts the customer entity.
+            //TableOperation insertOperation = TableOperation.Insert(botApp1);
+
+            // Execute the insert operation.
+            //table.ExecuteAsync(insertOperation);
+
+            // Create a retrieve operation that takes a customer entity.
+            TableOperation retrieveOperation = TableOperation.Retrieve<AppEntity>(ConfigurationManager.AppSettings["MicrosoftAppId"], ConfigurationManager.AppSettings["MicrosoftAppPassword"]);
+
+            // Execute the retrieve operation.
+            TableResult retrievedResult = table.ExecuteAsync(retrieveOperation).Result;
+
+            return Task.FromResult(this.Credentials.ContainsKey(appId));
+        }
+
+        public Task<string> GetAppPasswordAsync(string appId)
+        {
+            return Task.FromResult(this.Credentials.ContainsKey(appId) ? this.Credentials[appId] : null);
+        }
+
+        public Task<bool> IsAuthenticationDisabledAsync()
+        {
+            return Task.FromResult(!this.Credentials.Any());
+        }
+    }
+
+    /// Use the MultiCredentialProvider as credential provider for BotAuthentication
+    [BotAuthentication(CredentialProviderType = typeof(MultiCredentialProvider))]
+
     public class MessagesController : ApiController
     {
+        static MessagesController()
+        {
+
+            // Update the container to use the right MicorosftAppCredentials based on
+            // Identity set by BotAuthentication
+            var builder = new ContainerBuilder();
+
+            builder.Register(c => ((ClaimsIdentity)HttpContext.Current.User.Identity).GetCredentialsFromClaims())
+                .AsSelf()
+                .InstancePerLifetimeScope();
+            builder.Update(Conversation.Container);
+        }
+
+
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
