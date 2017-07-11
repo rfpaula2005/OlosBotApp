76858476d0d9535deab12b741c3023b5ee4c48e9
Rodrigo Fernandes de Paula <rodrigo.paula@olos.com.br>
2017-07-11 18:22:17 -0300

    App OK - 2017-07-11 18:22

--- OlosBotApp/Dialogs/RootDialog.cs
+++ OlosBotApp/Dialogs/RootDialog.cs
@@ -94,7 +94,7 @@ namespace OlosBotApp.Dialogs
                                 }
                             }
                         }
-                    }
+                    } 
                     */
 
                     string content;
